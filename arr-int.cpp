7d5e5cce3e7900c386a099832b2466c1769efa9f
rajiv-williams <59885073+rajiv-williams@users.noreply.github.com>
2020-03-12 12:13:13 -0400

    Update arr-int.cpp

--- arr-int.cpp
+++ arr-int.cpp
@@ -2,44 +2,45 @@
 #include <cstdlib>
 #include <iostream>
 using namespace std;
-
+template<class T>
 class DynamicArray 
 {
 public:
   DynamicArray();
   ~DynamicArray();
-  void addEntry(int newEntry);
-  int getEntry(int index);
+  void addEntry(T newEntry);
+  T getEntry(int index);
   int getSize();
 
 private:
-  int *dynamicArray;
+  T *dynamicArray;
   int size;
 };
-
-DynamicArray::DynamicArray()
+template<class T>
+DynamicArray<T>::DynamicArray()
 {
   dynamicArray = nullptr;
   size = 0;
 }
 
-
-DynamicArray::~DynamicArray()
+template<class T>
+DynamicArray<T>::~DynamicArray()
 {
   if (dynamicArray != nullptr)
     delete[] dynamicArray;
 }
-
-int DynamicArray::getSize()
+template<class T>
+int DynamicArray<T>::getSize()
 {
   return size;
 }
 
-// Adds an entry to the dynamic array. 
-void DynamicArray::addEntry(int newEntry)
+// Adds an entry to the dynamic array.
+template<class T> 
+void DynamicArray<T>::addEntry(T newEntry)
 {
     // Create a new array, copy the contents of the old array, then delete it
-    int *newArray = new int[size + 1];
+    T *newArray = new T[size + 1];
     for (int i = 0; i < size; i++)
     {
     newArray[i] = dynamicArray[i];
@@ -56,7 +57,8 @@ void DynamicArray::addEntry(int newEntry)
 
 
 // Retrieve the element at a given index
-int DynamicArray::getEntry(int index)
+template<class T>
+T DynamicArray<T>::getEntry(int index)
 {
   if ((index < 0) || (index >= size)){
     cout << "Out of the index !" << endl;
@@ -69,7 +71,7 @@ int DynamicArray::getEntry(int index)
 
 int main()
 {
-  DynamicArray x;
+  DynamicArray<int> x;
 
   // Adding entries
   x.addEntry(7);
@@ -83,7 +85,22 @@ int main()
   
   for (int i = 0; i < x.getSize(); i++)
     cout << x.getEntry(i) << endl;
+
+
+  DynamicArray<string> y;
+
+  // Adding entries
+  y.addEntry("Frank");
+  y.addEntry("Wiggum");
+  y.addEntry("Nahasapeemapetilon");
+  y.addEntry("Quimby");
+  y.addEntry("Flanders");
+
+  // Output entries
+  cout << "List:" << endl;
+  
+  for (int i = 0; i < y.getSize(); i++)
+    cout << y.getEntry(i) << endl;
   
   return 0;
 }
-
